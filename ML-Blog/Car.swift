f54a13c3fd7ba7033895638b2e9484e9399cca35
Daniel Radshun <daniel@msapps.mobi>
2019-12-04 10:04:29 +0200

    Models added

---
+++ ML-Blog/Car.swift
@@ -0,0 +1,15 @@
+//
+//  Car.swift
+//  ML-Blog
+//
+//  Created by Daniel Radshun on 03/12/2019.
+//  Copyright © 2019 Daniel Radshun. All rights reserved.
+//
+
+import Foundation
+
+struct Car {
+    let carYear:Double
+    let engine:Double
+    let destinationPassed:Double
+}
